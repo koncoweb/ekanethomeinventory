76b11210287a1a6765b0ceb5877f56f3489f84da
[dyad] <koncomyid@gmail.com>
2025-07-01 00:10:32 +0700

    [dyad] Removed duplicate Toaster component from main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"; // Import Toaster
 
 createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <Toaster /> {/* Tambahkan Toaster di sini */}
-  </>
+  <App />
 );
