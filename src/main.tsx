5688f6ba3fefa920afecad139a32c52586ab570c
[dyad] <koncomyid@gmail.com>
2025-07-01 00:08:51 +0700

    [dyad] Added Toaster component to main application entry point - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster"; // Import Toaster
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Toaster /> {/* Tambahkan Toaster di sini */}
+  </>
+);
